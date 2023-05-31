8b4bc321a78f01b09dca818640ee94ad343ea3e6
Szücs Gábor <gabor.balint66@gmail.com>
2023-05-31 19:49:50 +0200

    CI / CD pipeline HIBAJAV

--- data/src/main/java/hu/nye/progkor/restaurant/model/FoodType.java
+++ data/src/main/java/hu/nye/progkor/restaurant/model/FoodType.java
@@ -3,7 +3,7 @@ package hu.nye.progkor.restaurant.model;
 /** A menüben is választható étel típusokat tartalmazó FoodType típus */
 public enum FoodType {
 
-    PIZZA_FOOD("Pizza"),           /** Pizza kategóriába tartozó étel */
+    PIZZA_FOOD("Pizza"),           /** Pizza kategóriába tartozó étel test */
     OTHER_FOOD("Egyéb");           /** Gyrosok, hamburgerek és egyéb ételek */
 
     private final String value;
